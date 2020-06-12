932fb4579d47b9abad00b90395890a5ad01f3ce9
gbubemismith <gsmithwalter@gmail.com>
2020-06-12 13:55:09 +0100

    first commit

---
+++ DataStructures/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
